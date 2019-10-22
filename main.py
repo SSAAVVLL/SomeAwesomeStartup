df1930b1f8bf93808eff311bd0feb25962289144
SSAAVVLL <SSAAVVLL@bk.ru>
2019-10-22 13:19:59 +0500

    fixed template names

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
 
 @app.route('/')
-def hello():
-    return render_template('index')
+def index():
+    return render_template('index.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
 
 app.run()
