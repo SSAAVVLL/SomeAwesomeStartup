ea3f1d4702fbf226b18d434dae67b6f0e30b2f34
SSAAVVLL <SSAAVVLL@bk.ru>
2019-10-22 14:13:35 +0500

    First variant of markup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__,
+            template_folder='templates',
+            static_url_path='',
+            static_folder='static')
 
 
 @app.route('/')
