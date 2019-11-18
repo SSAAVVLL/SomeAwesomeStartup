20cb6c6f1868c9277d33f3b41862002b79f0e081
Alexander Kolbeko <sasha-kolbeko@mail.ru>
2019-11-18 20:57:19 +0500

    comment ready

--- filmood/parser/IMDB_parser.py
+++ filmood/parser/IMDB_parser.py
@@ -10,5 +10,9 @@ for id in range(1, 10000):
     json_imdbFilmId = json.loads(filmFromDb)
     json_imdbFilmId = json_imdbFilmId['imdb_id']
     print(json_imdbFilmId)
-    movie = filmImdbInfo.get_movie_external_reviews(movieID=json_imdbFilmId)
-    print(movie)
+    movie = filmImdbInfo.get_movie_reviews(json_imdbFilmId[2:])
+    counter = 0
+    for review in movie['data']['reviews']:
+        print(f"{review['content']} \n {20*'_'}")
+        counter += 1
+    print(f"amount of comments {counter} and this film id {json_imdbFilmId}")
