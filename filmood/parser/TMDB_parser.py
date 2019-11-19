8e2bc22c73b721d558a1189fdf8dbcf0f703b3d0
erik <errik25@gmail.com>
2019-11-19 19:28:11 +0500

    added db value validation

--- filmood/parser/TMDB_parser.py
+++ filmood/parser/TMDB_parser.py
@@ -24,32 +24,37 @@ for target_i in target_amounts:
     while amount_of_films < target_i:
         try:
             movie_info = tmdb.Movies(cur_id).info()
-            film = Film(
-                title = movie_info['title'],
-                release_date = datetime.strptime(movie_info['release_date'], '%Y-%m-%d'),
-                runtime = movie_info['runtime'],
-                backdrop_path = movie_info['backdrop_path'],
-                poster_path = movie_info['poster_path'],
-                imdb_id = movie_info['imdb_id'],
-                overview = movie_info['overview'],
-                vote_average = movie_info['vote_average']
-            )
+            if (movie_info['title'] and
+                    movie_info['release_date'] and
+                    movie_info['runtime'] and
+                    movie_info['backdrop_path'] and
+                    movie_info['poster_path'] and
+                    movie_info['imdb_id'] and
+                    movie_info['overview'] and
+                    movie_info['vote_average']):
+                film = Film(
+                    title = movie_info['title'],
+                    release_date = datetime.strptime(movie_info['release_date'], '%Y-%m-%d'),
+                    runtime = movie_info['runtime'],
+                    backdrop_path = movie_info['backdrop_path'],
+                    poster_path = movie_info['poster_path'],
+                    imdb_id = movie_info['imdb_id'],
+                    overview = movie_info['overview'],
+                    vote_average = movie_info['vote_average']
+                )
 
-            for genre in movie_info['genres']:
-                find_genre = Genre.query.filter_by(name=genre['name']).first()
-                if find_genre:
-                    film.genres.append(find_genre)
-                else:
-                    new_genre = Genre(name=genre['name'])
-                    film.genres.append(new_genre)
-            try:
+                for genre in movie_info['genres']:
+                    find_genre = Genre.query.filter_by(name=genre['name']).first()
+                    if find_genre:
+                        film.genres.append(find_genre)
+                    else:
+                        new_genre = Genre(name=genre['name'])
+                        film.genres.append(new_genre)
+
                 db.session.add(film)
                 db.session.commit()
-            except Exception:
-                db.session.rollback()
-                db.logger.error('...')
 
-            amount_of_films += 1
+                amount_of_films += 1
 
         except Exception as err:
             print(err)
